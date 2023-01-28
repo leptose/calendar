5111d54f904d900e9303b4a87824d82054246165
leptose <leptose@gmail.com>
2023-01-28 17:21:54 +0900

    calendar.java modify

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,21 +1,47 @@
+import java.util.Scanner;
+
 public class Calendar {
     public static void main(String[] args){
-        System.out.println("일  월 화  수 목  금 토");
-        System.out.println("---------------------");
-        for (int i=0;i<4;i++){
-            for(int j=1;j<8;j++){
-                if(i<2&&j==1){
-                    System.out.print(" ");
-                }
-                else if(i*7+j<10){
-                    System.out.print("  ");
-                }
-                System.out.print(j+i*7);
-                if(i*7+j>=9){
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+//        System.out.println("일  월 화  수 목  금 토");
+//        System.out.println("---------------------");
+//        for (int i=0;i<4;i++){
+//            for(int j=1;j<8;j++){
+//                if(i<2&&j==1){
+//                    System.out.print(" ");
+//                }
+//                else if(i*7+j<10){
+//                    System.out.print("  ");
+//                }
+//                System.out.print(j+i*7);
+//                if(i*7+j>=9){
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//       1  2  3  4  5  6  7  8  9 10 11 12
+//      31 28 31 30 31 30 31 31 30 31 30 31
+        Scanner scanner=new Scanner(System.in);
+        int month=scanner.nextInt();
+        int[] monthDay={31,28,31,30,31,30,31,31,30,31,30,31};
+//        if(month%2==0){
+//            if(month==2){
+//                System.out.println(month+"월은 28일까지 있습니다.");
+//            }
+//            else if(month<7){
+//                System.out.println(month+"월은 30일까지 있습니다.");
+//            }
+//            else{
+//                System.out.println(month+"월은 31일까지 있습니다.");
+//            }
+//        }
+//        else{
+//            if(month<7){
+//                System.out.println(month+"월은 31일까지 있습니다.");
+//            }
+//            else{
+//                System.out.println(month+"월은 30일까지 있습니다.");
+//            }
+//        }
+        System.out.println(month+"월은 "+monthDay[month-1]+"까지 있습니다.");
     }
 }
