d4b36a0f671520668136888d9ff5e02fc9c1c62e
leptose <leptose@gmail.com>
2023-01-29 00:04:39 +0900

    calendar.java modify

--- src/Calendar.java
+++ src/Calendar.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class Calendar {
     private static final int[] MAX_DAYS={31,28,31,30,31,30,31,31,30,31,30,31};
+    private static final int[] LEAP_MAX_DAYS={31,29,31,30,31,30,31,31,30,31,30,31};
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         int month;
+        int year;
         Calendar calendar=new Calendar();
         while(true){
             System.out.println("월을 입력하세요.");
             System.out.print("> ");
+            year=scanner.nextInt();
             month=scanner.nextInt();
             if(month==-1){
                 break;
@@ -18,7 +21,7 @@ public class Calendar {
             else if(month>12){
                 continue;
             }
-            calendar.normalCalendar(month);
+            calendar.normalCalendar(year,month);
 
         }
 //        System.out.println("일  월 화  수 목  금 토");
@@ -76,14 +79,20 @@ public class Calendar {
 
     }
 
-    public int getMaxDays(int month) {
-        return MAX_DAYS[month-1];
+    public int getMaxDays(int year,int month) {
+        boolean leapYear=isLeapYear(year);
+        if(leapYear){
+            return LEAP_MAX_DAYS[month-1];
+        }
+        else {
+            return MAX_DAYS[month - 1];
+        }
     }
 
-    public void normalCalendar(int month){
+    public void normalCalendar(int year,int month){
         System.out.println("일  월 화  수 목  금 토");
         System.out.println("---------------------");
-        int maxDay=getMaxDays(month);
+        int maxDay=getMaxDays(year,month);
         for(int i=1;i<=maxDay;i++){
             if(i<10){
                 if((i-1)%7==0){
@@ -101,13 +110,12 @@ public class Calendar {
                 System.out.println();
             }
         }
-        if(month!=2) {
+        if(month!=2||maxDay==29) {
             System.out.println();
         }
-//        System.out.println(" 1  2  3  4  5  6  7");
-//        System.out.println(" 8  9 10 11 12 13 14");
-//        System.out.println("15 16 17 18 19 20 21");
-//        System.out.println("22 23 24 25 26 27 28");
+    }
 
+    public boolean isLeapYear(int year){
+        return year%4==0&&(year%100!=0||year%400==0);
     }
 }
