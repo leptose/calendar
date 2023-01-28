c2feb84fa8dd063d3eddfbd87a43d26403ae2052
leptose <leptose@gmail.com>
2023-01-28 16:57:31 +0900

    28th calendar

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,5 +1,21 @@
 public class Calendar {
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("일  월 화  수 목  금 토");
+        System.out.println("---------------------");
+        for (int i=0;i<4;i++){
+            for(int j=1;j<8;j++){
+                if(i<2&&j==1){
+                    System.out.print(" ");
+                }
+                else if(i*7+j<10){
+                    System.out.print("  ");
+                }
+                System.out.print(j+i*7);
+                if(i*7+j>=9){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
